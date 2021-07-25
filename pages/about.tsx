96ae9c9aa9cecb68b68eec9bceba15421aef9144
unknown <andreagutierrez160983@gmail.com>
2021-07-25 02:28:25 +0000

    colors btn

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,16 @@ export default function AboutPage() {
   return (
     <Layout title="This is a learning app">
       <h1>About</h1>
-      <p>This is an app to learn</p>
+      <p>This is an app to learn,What exactly is next?
+          Well, what next does it allows for your react application to actually be rendered on the server on
+          So it's categorized as a react front end development framework.
+          So this is the main project. it´s called -----
+          And as far as our stack, as far as our technologies, we're going to be using, of course, next.js,
+          that's our main technology.
+
+          And then we have satrapy for the back end.
+          So Satrapy is a headless content management system.
+          From here, we can manage website users and also the events, the media, the permissions and all that.</p>
       <p>Version: 1.0.0</p>
     </Layout>
   );
