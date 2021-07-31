5c5304a57c1109fcdb081928e310787cba4189fe
Paula Nerenberg <paulita.glam@gmail.com>
2021-07-31 15:35:38 +0000

    SuperSimplePage css ready

--- components/LessonPageHeader.tsx
+++ components/LessonPageHeader.tsx
@@ -14,6 +14,66 @@ interface Props {
 }
 
 const LessonPageHeader: React.FC<Props> = ({ progress, slug }: Props) => {
+  const [drawerIsOpen, setdrawerIsOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState<boolean>(false);
+  const { setCurrentPageSlug } = useContext(AuthContext);
+
+  const router = useRouter();
+
+  const openDrawer = (): void => {
+    setdrawerIsOpen(true);
+  };
+
+  const closeDrawer = (): void => {
+    setdrawerIsOpen(false);
+  };
+
+  //when close the lesson without completing it then save the page slug
+  const closePage = (): void => {
+    setIsClosing(true);
+    setCurrentPageSlug(`lessons/${slug}`);
+    router.push("/courses");
+  };
+
+  return (
+    <>
+      {drawerIsOpen && <Backdrop selector="#backdrop" notifyClick={closeDrawer}></Backdrop>}
+      <SideDrawer show={drawerIsOpen} selector="#side-drawer">
+        <HelpLinks />
+      </SideDrawer>
+      <div className={styles.headerCenter}>
+        <div className={styles.topContainer}>
+          <div className={styles.infoButton}>
+            <Button circular color="blue" size="large" icon="info" onClick={openDrawer} />
+          </div>
+          <div className={styles.closeButton}>
+            <Button circular color="red" size="large" icon="close" loading={isClosing} onClick={() => closePage()} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LessonPageHeader;
+
+/*
+import { useState, useContext } from "react";
+import { useRouter } from "next/router";
+import { Grid, Button, Progress } from "semantic-ui-react";
+import Backdrop from "@components/Backdrop";
+import SideDrawer from "@components/SideDrawer";
+import HelpLinks from "@components/HelpLinks";
+import AuthContext from "@context/AuthContext";
+
+import styles from "@styles/LessonPageHeader.module.css";
+
+interface Props {
+  progress: number;
+  slug: string;
+}
+
+const LessonPageHeader: React.FC<Props> = ({ progress, slug }: Props) => {
 
   const [drawerIsOpen, setdrawerIsOpen] = useState(false);
   const [isClosing, setIsClosing] = useState<boolean>(false);
@@ -65,3 +125,6 @@ const LessonPageHeader: React.FC<Props> = ({ progress, slug }: Props) => {
 };
 
 export default LessonPageHeader;
+
+
+*/
--- components/SuperSimplePage.tsx
+++ components/SuperSimplePage.tsx
@@ -12,7 +12,6 @@ interface Props {
 }
 
 const SuperSimplePage: React.FC<Props> = ({ page, navSlugs }: Props) => {
-  
   const [feedback] = useState<Feedback>(Feedback.None);
   //setFeedback removed as it is not used in this demo template and that causes a TypeScript error.
   //const [feedback, setFeedback] = useState<Feedback>(Feedback.None);
@@ -35,13 +34,144 @@ const SuperSimplePage: React.FC<Props> = ({ page, navSlugs }: Props) => {
       feedback={feedback}
       notifyCannotContinue={() => {}}
     >
-{/*   Demonstrating how feedback can be set in the footer:
+      {/*   Demonstrating how feedback can be set in the footer:
 
       <button onClick={() => setFeedback(Feedback.Correct)}>Send Correct</button>
       <button onClick={() => setFeedback(Feedback.Incorrect)}>Send Wrong</button>
       <button onClick={() => setFeedback(Feedback.Hide)}>Hide</button> */}
       <div className={styles.audioImageContainer}>
         <AudioImage imageUrl={imageUrl} audioUrl={audioUrl}></AudioImage>
+      </div>
+      <div className={styles.mainContent}>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
+          repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+          praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+          eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
+          sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint?
+          Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+          Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
+          tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima
+          excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+          cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
+          ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas
+          quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
+          repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+          praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+          eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
+          sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint?
+          Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+          Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
+          tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima
+          excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+          cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
+          ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas
+          quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
+          repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+          praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+          eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
+          sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint?
+          Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+          Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
+          tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima
+          excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+          cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
+          ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas
+          quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
+          repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+          praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+          eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
+          sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint?
+          Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+          Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
+          tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima
+          excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+          cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
+          ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas
+          quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
+          repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+          praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+          eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
+          sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint?
+          Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+          Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
+          tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima
+          excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+          cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
+          ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas
+          quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
+          repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+          praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+          eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
+          sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint?
+          Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+          Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
+          tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima
+          excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+          cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
+          ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas
+          quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
+          repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+          praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+          eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
+          sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint?
+          Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+          Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
+          tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima
+          excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+          cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
+          ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas
+          quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
+          repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
+          praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+          eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
+          sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint?
+          Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+          Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
+          tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima
+          excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+          cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
+          ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas
+          quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
+        </p>
       </div>
     </LessonPageWrapper>
   );
