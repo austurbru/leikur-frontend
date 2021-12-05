5095e09e1567158aa8f614390abe623f8e2a2a4d
aegirorn <41763474+aegirorn@users.noreply.github.com>
2021-12-05 20:59:03 +0000

    Added Austurbru logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,30 @@
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 import styles from "@styles/Footer.module.css";
+import Image from "next/image";
+import logo from "../public/images/austurbru-logo.png";
 
 const Footer = () => {
   let { t } = useTranslation();
   return (
     <footer className={styles.footer}>
-            <button className="ui circular facebook icon button">
-  <i className="facebook icon"></i>
-</button>
-<button className="ui circular twitter icon button">
-  <i className="twitter icon"></i>
-</button>
-<button className="ui circular linkedin icon button">
-  <i className="linkedin icon"></i>
-</button>
-<button className="ui circular google plus icon button">
-  <i className="google plus icon"></i>
-</button>
+      <button className="ui circular facebook icon button">
+        <i className="facebook icon"></i>
+      </button>
+      <button className="ui circular twitter icon button">
+        <i className="twitter icon"></i>
+      </button>
+      <button className="ui circular linkedin icon button">
+        <i className="linkedin icon"></i>
+      </button>
+      <button className="ui circular google plus icon button">
+        <i className="google plus icon"></i>
+      </button>
       <p>&copy; Austurbrú 2021</p>
       <p>
         <Link href="/about">{t("common:aboutFooter")}</Link>
       </p>
+      <Image src={logo} alt="Austurbrú logo" width={50} height={50} />
     </footer>
   );
 };
